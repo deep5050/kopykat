e84e3e158da9dbd0f9177d9e2ffbb74958e9af76
Restyled.io <commits@restyled.io>
2020-06-09 06:58:51 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -4,15 +4,13 @@
 const clipboard = require('clipboardy');
 const notebook = require('./src/notebook');
 
-
 var last_clip = "";
 try {
-    last_clip = clipboard.readSync();
+  last_clip = clipboard.readSync();
 
 } catch (error) {
-    return new Error('could not read the clipboard');
+  return new Error('could not read the clipboard');
 }
-
 
 console.log("Service Started ... (Hit CTRL^C Twice To Force Quit)");
 notebook.initiate();
@@ -20,31 +18,29 @@ notebook.initiate();
 var i = 1;
 var inactive = 0;
 
-
 while (i < 1000) {
-    setTimeout(() => {
-        
-        var text = "";
-        try {
-            text = clipboard.readSync();
+  setTimeout(() => {
+    var text = "";
+    try {
+      text = clipboard.readSync();
 
-        } catch (error) {
-            return new Error('could not read the clipboard');
-        }
+    } catch (error) {
+      return new Error('could not read the clipboard');
+    }
 
-        if (text !== last_clip) {
-            notebook.write(text);
-            inactive = 0;
-        } else {
-            inactive++;
-            //console.log(`inactive ${inactive}`);
-            if (inactive >= 3000) {
-                console.log(`Quitting On Inactivity`);
-                process.exit(0);
-            }
-        }
-        last_clip = text;
-    }, 1000);
-    //console.log(`i ${i}`);
-    i++;
+    if (text !== last_clip) {
+      notebook.write(text);
+      inactive = 0;
+    } else {
+      inactive++;
+      // console.log(`inactive ${inactive}`);
+      if (inactive >= 3000) {
+        console.log(`Quitting On Inactivity`);
+        process.exit(0);
+      }
+    }
+    last_clip = text;
+  }, 1000);
+  // console.log(`i ${i}`);
+  i++;
 }
