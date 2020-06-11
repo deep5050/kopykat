923d2bb3a502e1c7895c4617f62305885831605a
Dipankar Pal <dipankarpal5050@gmail.com>
2020-06-11 12:33:09 +0530

    style: XO refactor

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
-'use strict'
+'use strict';
 
 const ClipboardListener = require('clipboard-listener');
 const notebook = require('./src/notebook');
 const copy = require('copy-paste');
 
-var last_copy = "";
+let last_copy = '';
 try {
-    last_copy = copy.paste();
-} catch (error) {
-    return new Error('could not read the clipboard');
+	last_copy = copy.paste();
+} catch {
+	console.log('Sorry, Could not read the clipboard :(');
+	process.exit(1);
 }
-console.log("Service Started ... (Hit CTRL^C Twice To Force Quit)");
+
+console.log('Service Started ... (Hit CTRL^C Twice To Force Quit)');
 
 notebook.initiate();
 
 const listener = new ClipboardListener({
-    timeInterval: 100, // Default to 250
-    immediate: true, // Default to false
+	timeInterval: 100, // Default to 250
+	immediate: true // Default to false
 });
 
 listener.on('change', value => {
-    if (value !== last_copy) {
-        last_copy = value;
-        notebook.write(value);
-    }
+	if (value !== last_copy) {
+		last_copy = value;
+		notebook.write(value);
+	}
 });
--- src/notebook.js
+++ src/notebook.js
@@ -1,10 +1,8 @@
-//@ts-check
+// @ts-check
 
 const detector = require('lang-detector');
 const fs = require('fs');
 const path = require('path');
-
-
 
 /**
  * Initiate the File. If the file doesn't exist write header
@@ -13,20 +11,18 @@ const path = require('path');
  * @returns {void}
  */
 module.exports.initiate = () => {
+	if (!fs.existsSync(path.join('.', 'kopykat.md'))) {
+		const dateObject = new Date();
+		const month = dateObject.getUTCMonth() + 1; // Months from 1-12
+		const day = dateObject.getUTCDate();
+		const year = dateObject.getUTCFullYear();
 
-    if (!fs.existsSync(path.join('.', 'kopykat.md'))) {
-        var dateObj = new Date();
-        var month = dateObj.getUTCMonth() + 1; //months from 1-12
-        var day = dateObj.getUTCDate();
-        var year = dateObj.getUTCFullYear();
+		const date = day + '/' + month + '/' + year;
+		const header = `# KOPYKAT ${date} \n## This Is An Auto-generated File`;
 
-        var date = day + "/" + month + "/" + year;
-        var header = `# KOPYKAT ${date} \n## This Is An Auto-generated File`;
-
-        fs.writeFileSync(path.join('.', 'kopykat.md'), header);
-    }
-
-}
+		fs.writeFileSync(path.join('.', 'kopykat.md'), header);
+	}
+};
 
 /**
  * Get the current time in HH:MM:SS AM/PM format
@@ -35,15 +31,13 @@ module.exports.initiate = () => {
  * @returns {string} - 12 Hour format current time HH:MM:SS AM/PM
  */
 module.exports.timestamp = () => {
-    return new Date(Date.now()).toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        hour12: true,
-        minute: 'numeric',
-        second: 'numeric'
-    })
-}
-
-
+	return new Date(Date.now()).toLocaleTimeString('en-US', {
+		hour: 'numeric',
+		hour12: true,
+		minute: 'numeric',
+		second: 'numeric'
+	});
+};
 
 /**
  * Identify the String and write with timestamp
@@ -52,28 +46,27 @@ module.exports.timestamp = () => {
  * @param {string}  data - Data to be written
  * @returns {void} - A Promise
  */
-module.exports.write = (data) => {
-    if (data.trim() == "") {
-        return;
-    }
-    var time = this.timestamp();
-    var text_to_write = "";
-    var lang = detector(data);
+module.exports.write = data => {
+	if (data.trim() == '') {
+		return;
+	}
 
-    if (lang === 'Unknown') {
-        //possibly, simple texts
-        data = data.trim();
-        text_to_write = `\n\n>**${time}** \n\n${data}\n`;
+	const time = this.timestamp();
+	let text_to_write = '';
+	const lang = detector(data);
 
-    } else {
-        // Possibly a programming snippet
-        if (lang !== 'Python') {
-            data = data.trim();
-        }
-        text_to_write = `\n\n>**${time}**\n\n\`\`\`${lang}\n${data}\n\`\`\``;
+	if (lang === 'Unknown') {
+		// Possibly, simple texts
+		data = data.trim();
+		text_to_write = `\n\n>**${time}** \n\n${data}\n`;
+	} else {
+		// Possibly a programming snippet
+		if (lang !== 'Python') {
+			data = data.trim();
+		}
 
+		text_to_write = `\n\n>**${time}**\n\n\`\`\`${lang}\n${data}\n\`\`\``;
+	}
 
-    }
-
-    fs.appendFileSync(path.join('.', 'kopykat.md'), text_to_write);
-}
+	fs.appendFileSync(path.join('.', 'kopykat.md'), text_to_write);
+};
